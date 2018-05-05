e01d859d6a6ca0355d84b1039468f54653f8ffe9
Paxman23l <gilbert.279@gmail.com>
2018-05-05 16:20:27 -0500

    trying again...4

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/schoolcloud.github.io/docs/',
+    assetsPublicPath: '../schoolcloud.github.io/docs/',
     
 
     /**
